96aeabd5aa772e7244ca3fe0372d566084d2d26f
Ong Yi Jie <ongyj-wp18@student.tarc.edu.my>
2020-11-07 17:14:22 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/assignment/MainActivity.kt
@@ -0,0 +1,11 @@
+package my.edu.tarc.assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
